c088d99d605ed68973f84831761cd815e4d90c96
RAJKUMARDASARI239067 <162983289+RAJKUMARDASARI239067@users.noreply.github.com>
2024-03-11 16:29:34 +0530

    Add files via upload

---
+++ file 3.py
@@ -0,0 +1,89 @@
+#print("hello")
+
+
+
+print("HELLO WORLD")
+"""
+
+
+-------> Commenting
+1.commenting is used for making documetation in a python file
+2.when the line is commented, that will not be considered as a part of the code
+3.when the program is excuted commented line will not be in the part of excution
+
+
+------>note;
+
+import this
+# import is akeyboard which is used to  communicateb b/w the python files.
+
+import antigravity
+
+11/3 1:00 to 5:00
+
+Bargained characters
+
+ Controls and Data structures-> ABC language
+ Strings and its formates-> from C
+ Regular expressions-> pearl
+ Exeption handling-> Module3
+ Magic methods from->fortron
+
+What kind of language is python is
+
+* Object oriented programming language
+* dynamically typed language
+*scripting language
+*inter preted language
+*compiled language
+*function programming lang language.
+
+Flavours of python
+
+
+-> Cpython-Written in C,Most common Implementation of python.
+-> Jython-Java implementation of python.
+-> Ironpython- implementation of the python programming language which is tithtly integrated with.NET.
+->Brython- Browser python, runs in browsers
+-> Rubypython-Bridge b/w python and ruby interpreters
+-> Micropython- runs in micro controllers.
+
+
+# print has 5 parameters
+# ----> print( object, sep, end,file, flush)
+# Object  --> the value to be printed is object
+# sep--> separator
+# Eg
+#a=8
+#b=9
+#c=89
+# print(a,b,c  sep='$'
+# end --> Which is used to add the string at the end of the value,
+# # It will not keep the command to stay in same line
+# Eg:
+# print(str(a), b, end='%%')
+# print( str(c))
+
+#! ---> variables
+# ?variable is a name Which is used to identify the value of variable
+#? Which is stored in soecific memory location
+
+
+# ! Rules to dedfine variables
+# 1.) Variable have  to start with only alphabets ,underscores
+# 2) the char following the first char can be alphabets , numbers , underscores
+# 3) no spaces are allowed
+# 4) no special charecters like !@#$%^&*()+
+# 5) case sensitive
+# 6) key words are not allowedas variables 
+
+# ! Ways to define variables
+
+# a=8
+# b=9
+# print(a,b)
+
+
+
+
+
